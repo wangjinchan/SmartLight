a2b05c980106c274f753d9d8183b232dc099310f
wangjinchan <wangjinchan_wjc@qq.com>
2020-11-27 17:23:48 +0800

    优化界面

--- app/src/main/java/com/example/myclient/TestFragment.java
+++ app/src/main/java/com/example/myclient/TestFragment.java
@@ -8,7 +8,6 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.StrictMode;
 import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +15,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import java.io.BufferedReader;
