8e149a4f4db831ed7a094d053b2ecca9fba1b0ed
wangjinchan <wangjinchan_wjc@qq.com>
2020-11-27 17:01:34 +0800

    优化socket连接；优化界面

--- app/src/main/java/com/example/myclient/TestFragment.java
+++ app/src/main/java/com/example/myclient/TestFragment.java
@@ -2,7 +2,7 @@ package com.example.myclient;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.os.Build;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -13,40 +13,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 
 public class TestFragment extends Fragment {
     private Button startButton;
     private EditText IPText;
     private Context mContext;
-    private boolean isConnecting=false;
+    private   boolean isConnecting=false;
     private Thread mThreadClient=null;
     private Socket mSocketClient=null;
     private static BufferedReader mBufferedReaderClient=null;
     private static PrintWriter mPrintWriterClient=null;
-    private static int a=1;
-    private String recvMessageClient="";
+    private  String res="";
     private static TextView recvText,recvText1,recvText2;
-    private Switch switch_c;
+    private View view;
     @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // TODO Auto-generated method stub
 
-        View view = inflater.inflate(R.layout.fragment_test, null);
+         view = inflater.inflate(R.layout.fragment_test, null);
         mContext=getContext();
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
                 .detectDiskReads()
@@ -62,7 +61,8 @@ public class TestFragment extends Fragment {
                 .build());
 
         IPText= view.findViewById(R.id.IPText);
-        IPText.setText("192.168.1.146:8080");
+      // IPText.setText("10.10.10.11:8080");
+        IPText.setText("192.168.1.127:8080");
         startButton= view.findViewById(R.id.StartConnect);
         startButton.setOnClickListener(StartClickListener);
 
@@ -75,105 +75,64 @@ public class TestFragment extends Fragment {
         recvText2= view.findViewById(R.id.textView4);
         recvText2.setMovementMethod(ScrollingMovementMethod.getInstance());
 
+        TempControlView tempControl = view.findViewById(R.id.temp_control);
+        // 设置三格代表温度1度
+        tempControl.setAngleRate(1);
+        tempControl.setTemp(0, 5, 0);
+        //设置旋钮是否可旋转
+        tempControl.setCanRotate(true);
 
-        switch_c=view.findViewById(R.id.switch_c);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            switch_c.setShowText(true);
-        }
-        switch_c.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        tempControl.setOnTempChangeListener(new TempControlView.OnTempChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked)
-                {
-                    switch_c.setSwitchTextAppearance(mContext,R.style.s_true);
-                    if(isConnecting&&mSocketClient!=null)
-                    {
-                        String msgText ="c1\n";//发送给单片机的某个命令
-                        try
-                        {
-                            mPrintWriterClient.print(msgText);
-                            mPrintWriterClient.flush();
-                        }catch (Exception e) {
-                            // TODO: handle exception
-                            Toast.makeText(mContext, "发送异常"+e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }else
-                    {
-                        Toast.makeText(mContext, "没有连接", Toast.LENGTH_SHORT).show();
-                    }
-
-                }else{
-                    switch_c.setSwitchTextAppearance(mContext,R.style.s_false);
-
-                    if(isConnecting&&mSocketClient!=null)
-                    {
-                        String msgText ="c0\n";//发送给单片机的某个命令
-                        try
-                        {
-                            mPrintWriterClient.print(msgText);
-                            mPrintWriterClient.flush();
-                        }catch (Exception e) {
-                            // TODO: handle exception
-                            Toast.makeText(mContext, "发送异常"+e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }else
-                    {
-                        Toast.makeText(mContext, "没有连接", Toast.LENGTH_SHORT).show();
-                    }
-
+            public void change(int temp) {
+                switch (temp){
+                    case 0:
+                        send("*C");
+                        break;
+                    case 1:
+                        send("*1");
+                        break;
+                    case 2:
+                        send("*2");
+                        break;
+                    case 3:
+                        send("*3");
+                        break;
+                    case 4:
+                        send("*4");
+                        break;
+                    case 5:
+                        send("*5");
+                        break;
                 }
             }
         });
 
-        final Switch switch_t=view.findViewById(R.id.switch_t);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            switch_t.setShowText(true);
-        }
-        switch_t.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        tempControl.setOnClickListener(new TempControlView.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked)
-                {
-                    switch_t.setSwitchTextAppearance(mContext,R.style.s_true);
-                    if(isConnecting&&mSocketClient!=null)
-                    {
-                        String msgText ="t1\n";//发送给单片机的某个命令
-                        try
-                        {
-                            mPrintWriterClient.print(msgText);
-                            mPrintWriterClient.flush();
-                        }catch (Exception e) {
-                            // TODO: handle exception
-                            Toast.makeText(mContext, "发送异常"+e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }else
-                    {
-                        Toast.makeText(mContext, "没有连接", Toast.LENGTH_SHORT).show();
-                    }
-
-                }else{
-                    switch_t.setSwitchTextAppearance(mContext,R.style.s_false);
-                    if(isConnecting&&mSocketClient!=null)
-                    {
-                        String msgText ="t0\n";//发送给单片机的某个命令
-                        try
-                        {
-                            mPrintWriterClient.print(msgText);
-                            mPrintWriterClient.flush();
-                        }catch (Exception e) {
-                            // TODO: handle exception
-                            Toast.makeText(mContext, "发送异常"+e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }else
-                    {
-                        Toast.makeText(mContext, "没有连接", Toast.LENGTH_SHORT).show();
-                    }
+            public void onClick(int temp) {
+                switch (temp){
+                    case 0:
+                        send("*C");
+                        break;
+                    case 1:
+                        send("*1");
+                        break;
+                    case 2:
+                        send("*2");
+                        break;
+                    case 3:
+                        send("*3");
+                        break;
+                    case 4:
+                        send("*4");
+                        break;
+                    case 5:
+                        send("*5");
+                        break;
                 }
             }
         });
-
-
-
         return view;
     }
 
@@ -185,40 +144,35 @@ public class TestFragment extends Fragment {
             // TODO Auto-generated method stub
             if(isConnecting)
             {
-                isConnecting = false;
-                try
+                isConnecting=false;
+                if(mSocketClient!=null)
                 {
-                    if(mSocketClient!=null)
-                    {
+                    try{
                         mSocketClient.close();
                         mSocketClient = null;
+                        if (mPrintWriterClient!=null){
+                            mPrintWriterClient.close();
+                            mPrintWriterClient = null;
+                        }
+                        mThreadClient.interrupt();
+                        startButton.setText("开始连接");
+                        IPText.setEnabled(true);//可以输入ip和端口号
+                        recvText.setText("断开连接\n");
 
-                        mPrintWriterClient.close();
-                        mPrintWriterClient = null;
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                }catch (IOException e) {
-                    // TODO: handle exception
-                    e.printStackTrace();
-                }
-                mThreadClient.interrupt();
 
-                startButton.setText("开始连接");
-                IPText.setEnabled(true);
-                recvText.setText("已经断开连接\n");
+                }
+
             }else
             {
-                isConnecting=true;
-                startButton.setText("停止连接");
-                recvText.setText("已经连接到智能衣柜！\n");
-                IPText.setEnabled(false);
                 mThreadClient = new Thread(mRunnable);
                 mThreadClient.start();
             }
         }
     };
 
-
-    //Ïß³Ì£º¼àÌý·þÎñÆ÷·¢À´µÄÏûÏ¢
     private Runnable mRunnable = new Runnable() {
 
         @Override
@@ -226,18 +180,16 @@ public class TestFragment extends Fragment {
             String msgText = IPText.getText().toString();
             if(msgText.length()<=0)
             {
-                recvMessageClient="IP和端口号不能为空！\n";//消息换行
                 Message msg = new Message();
-                msg.what = 1;
+                msg.what = 5;
                 mHandler.sendMessage(msg);
                 return;
             }
             int start = msgText.indexOf(":");
             if((start==-1)||(start+1>=msgText.length()))
             {
-                recvMessageClient ="IP地址不合法\n";
                 Message msg = new Message();
-                msg.what = 1;
+                msg.what = 6;
                 mHandler.sendMessage(msg);
                 return;
             }
@@ -245,45 +197,43 @@ public class TestFragment extends Fragment {
             String sPort = msgText.substring(start+1);
             int port = Integer.parseInt(sPort);
 
-
             try
             {
                 //连接服务器
-                mSocketClient = new Socket(sIP,port);
+                mSocketClient = new Socket();
+                SocketAddress socAddress = new InetSocketAddress(sIP, port);
+                mSocketClient.connect(socAddress, 2000);//设置超时时间为2秒
                 //取得输入、输出流
                 mBufferedReaderClient=new BufferedReader(new InputStreamReader(mSocketClient.getInputStream()));
                 mPrintWriterClient=new PrintWriter(mSocketClient.getOutputStream(),true);
-                recvMessageClient=" ";
-
                 Message msg = new Message();
                 msg.what = 1;
                 mHandler.sendMessage(msg);
+
             }catch (Exception e) {
-                recvMessageClient = "连接IP异常:" + e.toString() + e.getMessage() + "\n";//消息换行
                 Message msg = new Message();
-                msg.what = 1;
+                msg.what = 2;
                 mHandler.sendMessage(msg);
                 return;
             }
-
             char[] buffer = new char[256];
             int count = 0;
-            while(isConnecting)
+
+            while(true)
             {
                 try
                 {
                     if((count = mBufferedReaderClient.read(buffer))>0)
                     {
-                        recvMessageClient = getInfoBuff(buffer,count)+"\n";
+                        res = getInfoBuff(buffer,count)+"\n";
                         Message msg = new Message();
-                        msg.what = 1;
+                        msg.what = 4;
                         mHandler.sendMessage(msg);
                     }
                 }catch (Exception e) {
                     // TODO: handle exception
-                    recvMessageClient = "接收异常:" + e.getMessage() + "\n";//消息换行
                     Message msg = new Message();
-                    msg.what = 1;
+                    msg.what = 3;
                     mHandler.sendMessage(msg);
                 }
             }
@@ -297,30 +247,49 @@ public class TestFragment extends Fragment {
         public void handleMessage(Message msg)
         {
             super.handleMessage(msg);
-            if(msg.what==1)
+            if(msg.what==4)
             {
                 char []arrs=null;
-                while (a>0){
-                    recvText.append(recvMessageClient);//刷新
-                    a--;
-                }
-                arrs=recvMessageClient.toCharArray();
-                if (arrs[0]=='1'&&arrs[8]>='7'){
-                    recvText1.setText("温湿度: "+arrs[5] + arrs[6] + "℃" + ' ' + arrs[8] + arrs[9] + "%   差，请除湿");
-                }
-                else if (arrs[0]=='1'&&arrs[8]<='6'){
-                    recvText1.setText("温湿度: "+arrs[5] + arrs[6] + "℃" + ' ' + arrs[8] + arrs[9] + "%   良");
-                }
-                else if (arrs[0]=='2'&&arrs[6]<='3'){
-                    recvText2.setText("气体浓度："+arrs[5]+arrs[6]+arrs[7]+"%   良");
-                }
-                else if (arrs[0]=='2'&&arrs[5]>'3'){
-                    recvText2.setText("气体浓度："+arrs[5]+arrs[6]+arrs[7]+"%   差，请通风");
-                }
-            }
-        };
-    };
+                arrs=res.toCharArray();//接收来自服务器的字符串
 
+                if (arrs[0]=='T'){
+                    recvText1.setText("温度: "+arrs[3] + arrs[4] + "℃" + ' ' );
+                }else if (arrs[0]=='R'){
+                    recvText2.setText("湿度: "+arrs[3] + arrs[4] + "%" + ' ' );
+                }
+            }else if (msg.what==2){
+                showDialog("连接失败，服务器走丢了");
+                startButton.setText("开始连接");
+
+            }else if (msg.what==1){
+                showDialog("连接成功！");
+                recvText.setText("已连接衣柜\n");
+                IPText.setEnabled(false);//锁定ip地址和端口号
+                isConnecting = true;
+                startButton.setText("停止连接");
+            }else if (msg.what==3){
+              //  recvText.setText("连接已断开\n");
+            }else if (msg.what==5){
+                recvText.setText("IP和端口号不能为空\n");
+            }
+            else if (msg.what==6){
+                recvText.setText("IP地址不合法\n");
+            }
+        }
+    };
+    private  void showDialog(String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setIcon(android.R.drawable.ic_dialog_info);
+        builder.setTitle(msg);
+        builder.setCancelable(false);
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.create().show();
+    }
     private String getInfoBuff(char[] buff,int count)
     {
         char[] temp = new char[count];
@@ -329,4 +298,24 @@ public class TestFragment extends Fragment {
         }
         return new String(temp);
     }
+    private void send(String msg){
+        if(isConnecting&&mSocketClient!=null)
+                {
+                    String msgText =msg;//发送给单片机的某个命令
+                    try
+                    {
+                        mPrintWriterClient.print(msgText);
+                        mPrintWriterClient.flush();
+                    //    Toast.makeText(mContext,msg,Toast.LENGTH_SHORT).show();
+                    }catch (Exception e) {
+                        // TODO: handle exception
+                        Toast.makeText(mContext, "发送异常"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }else
+                {
+                    Toast.makeText(mContext, "没有连接", Toast.LENGTH_SHORT).show();
+                }
+    }
+
 }
+
--- app/src/main/java/com/example/myclient/adapter/ContentAdapter.java
+++ app/src/main/java/com/example/myclient/adapter/ContentAdapter.java
@@ -2,6 +2,8 @@ package com.example.myclient.adapter;
 
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 import java.util.List;
 
@@ -19,10 +21,11 @@ public class ContentAdapter extends PagerAdapter {
     }
 
     @Override
-    public boolean isViewFromObject(View arg0, Object arg1) {
+    public boolean isViewFromObject(View arg0, @NonNull Object arg1) {
         return arg0 == arg1;
     }
 
+    @NonNull
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
         View view = views.get(position);
@@ -31,7 +34,7 @@ public class ContentAdapter extends PagerAdapter {
     }
 
     @Override
-    public void destroyItem(ViewGroup container, int position, Object object) {
+    public void destroyItem(ViewGroup container, int position, @NonNull Object object) {
         container.removeView(views.get(position));
     }
 
